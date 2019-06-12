8aafe9a1c67c471887250c81420f076f4ae76a98
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-06-12 13:51:56 -0700

    High low game Completed

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -10,30 +10,45 @@ import javax.swing.JOptionPane;
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
+		// 3. Change this line to give you a random number between 1 - 100.
 		int random = new Random().nextInt(101);
-		
+
 		// 2. Print out the random variable above
 		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
-String head = JOptionPane.showInputDialog("Type your User");
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			int guess = (Integer.parseInt(head));
-			// 5. if the guess is correct
-			
-				// 6. Win
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-				// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
+ for (int i = 0; i < 10; i++) {
+	
 
+		// 1. Ask the user for a guess using a pop-up window, and save their response
+		String head = JOptionPane.showInputDialog("Type your number");
+		// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+		int guess = (Integer.parseInt(head));
+		// 5. if the guess is correct
+		if (guess == random) {
+
+			// 6. Win
+
+			System.out.println("Congrat you won!");
+		
+		// 12. Use "System.exit(0);" to quit the game if the user guessed the right
+		// answer.
+		System.exit(0); }
+		// 7. if the guess is high
+
+		else if (guess > random) {
+
+			// 8. Tell them it's too high
+			System.out.println("it's too high");
+		}
+
+		// 9. if the guess is low
+		else if (guess < random) {
+
+			// 10. Tell them it's too low
+			System.out.println("it's too low");
+		}
 		// 13. Tell them they lose
-		
+		System.out.println("GAME OVER! YOU LOSE!");
 	}
-
 }
-
-
+}
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -8,20 +8,20 @@ import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
 
-        //6. If the user doesn’t enter anything, choose a random color
+		// 3. Ask the user what color they would like the robot to draw
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+		// 5. Use an if/else statement to set the pen color that the user requested
 
+		// 6. If the user doesn’t enter anything, choose a random color
+
+		// 7. Put a loop around your code so that you keep asking the user for more
+		// colors & drawing them
+
+		// 4. Set the pen width to 10
+
+		// 2. Make the robot draw a shape (this will take more than one line of code)
 
 	}
 }
