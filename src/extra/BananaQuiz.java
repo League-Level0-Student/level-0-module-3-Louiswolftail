a6056dde1e4628ee9d73ced3c2bca81c65de2cfc
368325 <368325@0338S-S-R0B6H0K.sdcs.local>
2019-09-24 14:23:56 -0700

    100% Banana Quiz

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -10,10 +10,19 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String Banana = JOptionPane.showInputDialog("Do u like Bananas?");
 		//2. if they say no, 
+if(Banana.equals("no")) { 
+	JOptionPane.showMessageDialog(null, "U Hate Bananas, PP head!");
+	System.out.println("END QUIZ!!");
+}
 			//tell them they are crazy 
 			//and end quiz
 		//3. if they say yes
+else if(Banana.equals("yes")) {
+	String oofa = JOptionPane.showInputDialog("mmmmmmmmmmm! btw Whats your favorite hobby?");
+	JOptionPane.showMessageDialog(null, oofa +" is much better with bananas!");
+}
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
